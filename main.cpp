8916aa91e9390b23d97dc41e0dcd0b8b5dfff364
frantzhans <frantz.lecherbonnier@gmail.com>
2019-02-19 11:01:04 +0100

    update

--- main.cpp
+++ main.cpp
@@ -19,6 +19,34 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
+    #include <stdio.h>
+#include <stdlib.h>
+#include <winsock.h>
+#include <MYSQL/mysql.h>
+
+
+
+int main(void)
+{
+    MYSQL mysql;
+    //initialisation 
+    mysql_init(MYSQL *mysql);
+    mysql_options(&mysql,MYSQL_READ_DEFAULT_GROUP,"option");
+
+    /*localhost peut generer des erreurs*/
+    if(mysql_real_connect(&mysql,"localhost","mon_pseudo","******","ma_base",0,NULL,0))
+    {
+        mysql_close(&mysql);
+    }
+    else
+    {
+        printf("Une erreur s'est produite lors de la connexion à la BDD!");
+    }
+
+    return 0;
+}
+
+
 
     return 0;
 }
@@ -29,9 +57,9 @@ int main(int argc, char** argv) {
 
 
 
-/* Connexion / d�connexion */
-#include <winsock.h>
-#include <MYSQL/mysql.h>
+
+
+
 
 /*mysql_options :
 Le premier argument est un pointeur de structure, que nous avons vu juste avant.
@@ -50,8 +78,45 @@ MYSQL mysql;
 /*initialisons MySQL, gr�ce � la fonction*/
 mysql_init(MYSQL *mysql);
 
-/* initialisation r�alis�e, il faut se connecte */
+// initialisation r�alis�e, il faut se connecte 
 mysql_options(MYSQL *mysql, enum mysql_option option, const char *arg);
 mysql_real_connect(MYSQL *mysql, const char *host, const char *user, const char *passwd, const char *db, unsigned int port, const char *unix_socket, unsigned long client_flag);
 
+//la fonction permettant de faire des requêtes MySQL 
+mysql_query(MYSQL *mysql, const char *query);
 
+//spécifier pour cette fonction le pointeur de structures ainsi que la requête
+mysql_query(&mysql, "INSERT INTO ma_table VALUES('valeur 1', 'valeur 2', 'etc')");
+
+//savoir combien de lignes ont été affectées par votre requête, il suffit d'appeler la fonction :
+mysql_affected_rows(MYSQL *mysql);
+
+//pour récupérer le résultat il y a deux façons de faire:
+mysql_use_result(MYSQL *mysql)  //récupère le jeu de résultat, mais ne l'enregistre pas dans le client stocker la valeur de retour de cette fonction dans un pointeur de structure de type MYSQL_RES
+
+//Stocker un jeu de résultat ne suffit pas pour le lire ; appeler une autre fonction :
+mysql_fetch_row(MYSQL_RES *result);
+
+stocke les valeurs de la ligne suivante dans le jeu de résultats. Il est nécessaire, de libérer la mémoire allouée par cette fonction.
+mysql_free_result(MYSQL_RES *result);
+
+
+//requete selection table scores
+ mysql_query(&mysql, "SELECT * FROM scores");
+ //Déclaration des objets
+        MYSQL_RES *result = NULL;
+        MYSQL_ROW row;
+        int i = 1;
+         //On met le jeu de résultat dans le pointeur result
+        result = mysql_use_result(&mysql);
+        //Tant qu'il y a encore un résultat ...
+        while ((row = mysql_fetch_row(result)))
+        {
+           printf("Resultat %ld\n", i);
+           i++;
+        }
+        //Libération du jeu de résultat
+        mysql_free_result(result);
+
+        //Fermeture de MySQL
+        mysql_close(&mysql);
